ce6888efd6e740da2c483232d49bf6919b924d7a
haile <hailemix2@gmail.com>
2017-09-18 19:26:58 +0300

    The TableView has just begun!

---
+++ BestQuotes/MyCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MyCollectionViewCell.swift
+//  BestQuotes
+//
+//  Created by user on 9/18/17.
+//  Copyright © 2017 Hailemichael. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    
+
+    @IBOutlet weak var myImage: UIImageView!
+   
+    
+    
+}
--- BestQuotes/ViewController.swift
+++ BestQuotes/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource ,UICollectionViewDelegateFlowLayout{
 
+    @IBOutlet weak var MyCollectionView: UICollectionView!
+
+
+    var images = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n"]
+    let zCellIdentifier = "collectionCell"
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.MyCollectionView.delegate = self
+        self.MyCollectionView.dataSource = self
+        
+        
+        
+     
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    
+    {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    
+        return CGSize(width:view.frame.width/2, height:view.frame.height/3)
+        
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: zCellIdentifier, for: indexPath) as! MyCollectionViewCell
+        
+        cell.myImage.image = UIImage(named: images[indexPath.row])
+        return cell
+    }
+    
 
 }
 
